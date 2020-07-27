1dd0fb8a6dde551669e4b7ff7b694ba561d573a1
kathmontalvo <kathmontalvo123@gmail.com>
2020-07-26 23:57:23 -0500

    docs agregado

---
+++ docs/precache-manifest.e4c12c0ce57a3c5b30a9e1ab6535b739.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db20c0ddafc1f800e536be02163c2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b62bea3f8c463195c5",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "d7a0c4bdbb9fca109160",
+    "url": "/static/css/main.776ac36c.chunk.css"
+  },
+  {
+    "revision": "d5b62bea3f8c463195c5",
+    "url": "/static/js/2.775af3c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.775af3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a0c4bdbb9fca109160",
+    "url": "/static/js/main.9ffa13d1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
